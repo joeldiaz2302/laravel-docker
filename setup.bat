a4764ce93015194200fabe881261afcf3149705d
Joel Diaz <joeldiaz2302@gmail.com>
2019-07-12 20:06:19 -0500

    Added windows setup
    Push other changes from gitignore and .env.example

---
+++ setup.bat
@@ -0,0 +1,71 @@
+@ECHO off
+
+echo load the environment variables
+
+setlocal enableextensions enabledelayedexpansion
+
+if EXIST ".env" (
+  for /F "tokens=*" %%I in (.env) do @set %%I
+)
+echo DOCKER_LOCATION=%DOCKER_LOCATION%
+echo PROJECT_LOCATION=%PROJECT_LOCATION%
+
+
+echo build a docker instance for text editing script to run
+
+docker pull node:10
+echo docker run -dit -v "%DOCKER_LOCATION%":"%INSTALL_SRC%" -v "%PROJECT_LOCATION%":"%INSTALL_PROJ%" -w %INSTALL_SRC% --name nodeinstaller node
+docker run -dit -v "%DOCKER_LOCATION%":"%INSTALL_SRC%" -v "%PROJECT_LOCATION%":"%INSTALL_PROJ%" -w %INSTALL_SRC% --name nodeinstaller node
+docker exec nodeinstaller npm i
+
+echo docker-compose build
+docker-compose build
+echo docker-compose  up -d
+docker-compose up -d
+echo Waiting a few seconds to let the containers load up.
+timeout /t 4 
+echo docker ps - Showing the created containers
+docker ps
+timeout /t 4
+
+rem if [ "$NEW_PROJECT" == "build" ]; then
+rem 	if [ "$PROJECT_REPO" != "" ]; then
+rem 		echo docker exec app composer create-project --prefer-dist laravel/laravel tempapp --repository-url=$PROJECT_REPO
+rem 		docker exec app composer create-project --prefer-dist laravel/laravel tempapp --repository-url=$PROJECT_REPO
+rem 	else
+rem 		echo docker exec app composer create-project --prefer-dist laravel/laravel tempapp 
+rem 		docker exec app composer create-project --prefer-dist laravel/laravel tempapp
+rem 	fi
+rem 	echo docker exec app rsync -vua --delete-after tempapp/ .
+rem 	docker exec app rsync -vua --delete-after tempapp/ .
+rem fi
+
+docker exec nodeinstaller npm run setupEnvFile
+timeout /t 10
+
+echo list of databases
+docker exec db mysql -u root -p"%MYSQL_ROOT_PASSWORD%" -e "show databases;"
+docker exec db mysql -u root -p"%MYSQL_ROOT_PASSWORD%" -e "GRANT ALL PRIVILEGES ON %MYSQL_GRANT% TO %MYSQL_USER% IDENTIFIED BY '%USER_PASSWORD%';"
+docker exec db mysql -u root -p"%MYSQL_ROOT_PASSWORD%" -e "GRANT ALL PRIVILEGES ON %MYSQL_GRANT% TO %MYSQL_APPUSER% IDENTIFIED BY '%USER_PASSWORD%';"
+docker exec db mysql -u root -p"%MYSQL_ROOT_PASSWORD%" -e "FLUSH PRIVILEGES;"
+echo "GRANT ALL PRIVILEGES ON %MYSQL_GRANT% TO %MYSQL_USER% IDENTIFIED BY '%USER_PASSWORD%'; FLUSH PRIVILEGES;"
+timeout /t 4
+
+
+echo docker exec app composer install
+docker exec app composer install
+echo docker exec app php artisan key:generate
+docker exec app php artisan key:generate
+echo docker exec app php artisan config:cache
+docker exec app php artisan config:cache
+
+echo docker exec app php artisan migrate
+docker exec app php artisan migrate
+
+echo docker exec app php artisan db:seed
+docker exec app php artisan db:seed
+
+echo unsetting configuration values
+docker container stop nodeinstaller
+docker container rm nodeinstaller
+echo setup complete
